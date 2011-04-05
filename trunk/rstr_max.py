d602e9dbb0b97ca248c7b1ed6a0810fc7c945ae8
rbrixtel <rbrixtel@eeca0fbb-5747-c0a7-d927-5a0f84ede5af>
2011-04-05 13:42:41 +0000

    lighter, better

--- trunk/rstr_max.py
+++ trunk/rstr_max.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from tools_karkkainen_sanders import *
+from tools_karkkainen_sanders_v3 import direct_kark_sort as direct_kark_sort
 #from tools_karkkainen_sanders import *
 from array import array as array
 
@@ -11,11 +11,7 @@ def removeMany(end_, m, idxEnd, _lst, _max, _top, _results):
     n, idxStart = _lst.pop()
     end_ = _max.pop()
     if (prevStart != idxStart):
-      id_ = (end_, idxEnd-idxStart+1)
-      if(_results.has_key(id_) and _top > _results[id_][0]) :
-        _results[id_] = (_top, idxStart)
-      else :
-        _results[id_] = (_top, idxStart)
+      _results[(end_, idxEnd-idxStart+1)] = (_top, idxStart)
       prevStart = idxStart
     m -= n
     _top -= n
@@ -23,7 +19,7 @@ def removeMany(end_, m, idxEnd, _lst, _max, _top, _results):
     _max.append((end_[0],end_[1]-n-m))
     _lst.append((-m, idxStart))
     _top -= m
-  return _lst,_max,_top,_results
+  return _top
 
 class Rstr_max :
 
@@ -35,9 +31,8 @@ class Rstr_max :
   def add_str(self, str_unicode) :
     self.array_str.append(str_unicode)
     id_str = len(self.array_str) - 1
-    len_str = len(str_unicode)
 
-    for i in xrange(len_str) :
+    for i in xrange(len(str_unicode)) :
       self.array_suffix.append((i, id_str))
 
   def step1_sort_suffix(self) :
@@ -53,29 +48,11 @@ class Rstr_max :
         k += 1
       k += 1
 
-#    n = len(global_suffix)
-#    self.res = [0]*k
-#    global_suffix += char_final*3
-
-#    alphabet = [None] + sorted(set(global_suffix))
-#    k = len(alphabet)
-#    t = dict((c, i) for i,c in enumerate(alphabet))
-#    n = len(global_suffix)
-#    SA = array.array('i', [0]*(n+3))
-#    global_suffix = array.array('i', [t[c] for c in global_suffix]+[0]*3)
-#    kark_sort(global_suffix, SA, n, k)
-#    self.res = SA
     self.res = direct_kark_sort(global_suffix)
-#    s,self.res = simple_kark_sort(global_suffix)
-
-
-#    kark_sort(global_suffix, self.res, k, sorted(set(global_suffix)))
 
   def step2_lcp(self) :
     n = len(self.res)
-    c = n - len(self.array_str)
-
-    tmp = [0]*c
+    tmp = [0]*(n - len(self.array_str))
     rank = array('i',tmp)
     SA = array('i',tmp)
 
@@ -86,6 +63,8 @@ class Rstr_max :
       rank[key] = k
       SA[k] = key
       k += 1
+
+    self.distrib = None
 
     l = 0
     lcp = array('i',[0]*(k-1))
@@ -118,26 +97,26 @@ class Rstr_max :
     prev_len = 0
 
     idx = 0
+    len_lcp = len(self.lcp)
 #    for current_len in self.lcp :      
-    for idx in xrange(len(self.lcp)) :
+    for idx in xrange(len_lcp) :
       current_len = self.lcp[idx]
-      su1  = self.array_suffix[idx]
-      su2  = self.array_suffix[idx+1]
-      end_ = max((su1[1], su1[0]+current_len), (su2[1], su2[0]+current_len)) 
+      end_ = max((self.array_suffix[idx][1], self.array_suffix[idx][0]+current_len),
+                 (self.array_suffix[idx+1][1], self.array_suffix[idx+1][0]+current_len)) 
       if prev_len < current_len:
         cp = current_len - prev_len
         _max.append(end_)
         _lst.append((cp, idx))
         _top += cp
       elif prev_len > current_len:
-        _lst,_max,_top,_results = removeMany(end_, prev_len-current_len, idx, _lst, _max, _top, _results)
+        _top = removeMany(end_, prev_len-current_len, idx, _lst, _max, _top, _results)
       elif _top > 0 and end_ > _max[-1]:
         _max[-1] = end_
       prev_len = current_len
 #      idx += 1
 
     if _top > 0:
-      _lst,_max,_top,_results = removeMany(len(self.lcp)+1, _top, idx+1, _lst, _max, _top, _results)
+      _top = removeMany(len_lcp+1, _top, idx+1, _lst, _max, _top, _results)
 
     return _results
 
