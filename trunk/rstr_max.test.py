7df72523118854910bf74c7d7aa0896586e0b53f
rbrixtel <rbrixtel@eeca0fbb-5747-c0a7-d927-5a0f84ede5af>
2011-07-07 09:37:46 +0000

    debug rstr_max.py + unit test

--- trunk/rstr_max.test.py
+++ trunk/rstr_max.test.py
@@ -8,24 +8,24 @@ from rstr_max import *
 from string import *
 import sys
 
-str1 = "Otto von Bolschwing est mort sous le soleil de Sacramento (Californie), le 9 mars 1982. Il avait 72 ans et plusieurs vies derrière lui. Dans la première, ce rejeton de la noblesse allemande, membre des services de renseignement de la SS - l'ossature nazie du IIIe Reich - a exercé ses talents d'agent secret en Palestine. Devenu aide de camp du responsable des Affaires juives, Adolf Eichmann, il s'est attelé avec zèle à l'élaboration du programme d'extermination des juifs d'Europe. Après la guerre, le capitaine von Bolschwing a rejoint les rangs de la CIA, sous le nom de code de Grossbahn. En récompense de ses loyaux services, ses nouveaux maîtres lui ont offert une deuxième vie aux Etats-Unis, où l'ancien SS s'est taillé une belle carrière dans l'industrie. Jusqu'à devenir, en 1969, président de l'entreprise californienne de high-tech TCI."
-
+#str1 = "abc abc"
 #str1 = 'abc'
-
+str1 = open('./002.art').read()
 str1_unicode = unicode(str1,'utf-8','replace')
-str2_unicode = str1_unicode[::-1]
+#str2_unicode = str1_unicode[::-1]
 
 rstr = Rstr_max()
 rstr.add_str(str1_unicode)
-rstr.add_str(str2_unicode)
+#rstr.add_str(str2_unicode)
 r = rstr.go()
 
 for ((id_str, end), nb), (l, start_plage) in r.iteritems():
   ss = rstr.array_str[id_str][end-l:end]
-#  print ss
-  for o in range(start_plage, start_plage + nb) :
-    su = rstr.array_suffix[o]
+  if len(ss) > 8 :
+    print '[%s] %d'%(ss.encode('utf-8'), nb)
+#  for o in range(start_plage, start_plage + nb) :
+#    su = rstr.array_suffix[o]
 #    print '   ' + str(su)
-    sss = rstr.array_str[su[1]][su[0]:su[0]+l]
-    if ss != sss :
-      print ss, sss
+#    sss = rstr.array_str[su[1]][su[0]:su[0]+l]
+#    if ss != sss :
+#      print ss, sss
--- trunk/rstr_max.zipf.py
+++ trunk/rstr_max.zipf.py
@@ -9,10 +9,9 @@ import math
 
 str1 = "Otto von Bolschwing est mort sous le soleil de Sacramento (Californie), le 9 mars 1982. Il avait 72 ans et plusieurs vies derrière lui. Dans la première, ce rejeton de la noblesse allemande, membre des services de renseignement de la SS - l'ossature nazie du IIIe Reich - a exercé ses talents d'agent secret en Palestine. Devenu aide de camp du responsable des Affaires juives, Adolf Eichmann, il s'est attelé avec zèle à l'élaboration du programme d'extermination des juifs d'Europe. Après la guerre, le capitaine von Bolschwing a rejoint les rangs de la CIA, sous le nom de code de Grossbahn. En récompense de ses loyaux services, ses nouveaux maîtres lui ont offert une deuxième vie aux Etats-Unis, où l'ancien SS s'est taillé une belle carrière dans l'industrie. Jusqu'à devenir, en 1969, président de l'entreprise californienne de high-tech TCI."
 
-#str1 = open('Python.htm').read()
-
+str1 = open('002.art').read()
 str1_unicode = unicode(str1,'utf-8','replace')
-str2_unicode = str1_unicode[::-1]
+#str2_unicode = str1_unicode[::-1]
 
 rstr = Rstr_max()
 rstr.add_str(str1_unicode)
@@ -36,7 +35,7 @@ list_x = [math.log(elt[1]) for elt in l]
 list_y = [math.log(i) for i in range(1, len(list_x)+1)]
 
 plt.plot(list_x, list_y, 'r-')
-filename = 'zipf_rstr.png'
+filename = 'zipf_rstr.pdf'
 plt.xlabel('log(rank)')
 plt.ylabel('log(freq.)')
 plt.savefig(filename)
