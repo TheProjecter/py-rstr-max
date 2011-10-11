c55a42825eb9268756bd29e9772ea5dee1302532
rbrixtel <rbrixtel@gmail.com>
2011-10-11 16:24:11 +0000

    adapt rstr-max.test.py from previous modification on rstr-max.py

--- rstr_max.test.py
+++ rstr_max.test.py
@@ -9,7 +9,8 @@ from string import *
 import sys
 
 rstr = Rstr_max()
-str1 = 'a'*1000000 #'tototiti'
+str1 = 'tititoto'
+#str1 = 'aaaa'
 #str1 = str1 + 'b' + str1
 str1_unicode = unicode(str1,'utf-8','replace')
 rstr.add_str(str1_unicode)
@@ -20,6 +21,19 @@ rstr.add_str(str1_unicode)
 # str2_unicode = unicode(str2,'utf-8','replace')
 # rstr.add_str(str2_unicode)
 r = rstr.go()
+
+for (offset_end, nb), (l, start_plage) in r.iteritems():
+  ss = rstr.global_suffix[offset_end-l:offset_end]
+#  ss = rstr.array_str[id_str][end-l:end]
+  
+  print '[%s] %d'%(ss.encode('utf-8'), nb)
+  for o in range(start_plage, start_plage + nb) :
+    offset_global = rstr.res[o]
+    sss = rstr.global_suffix[offset_global:offset_global+l]
+    id_chaine = rstr.idxString[offset_global]
+    print '   ',sss
+#    ss = rstr.array_str[su[1]][su[0]:su[0]+l]
+
 
 print "res", len(r)
 
